005d2dbffb6edee25c20e9a1f5a8d949baf9cb7c
HEROhaoyu <2729903242@qq.com>
2024-03-25 15:38:18 +0800

    确保功能无误，已经实现了用户下单

--- sky-common/src/main/java/com/sky/exception/AddressBookBusinessException.java
+++ sky-common/src/main/java/com/sky/exception/AddressBookBusinessException.java
@@ -1,9 +1,9 @@
 package com.sky.exception;
 
-public class AddressBookBusinessException extends BaseException {
+public class AddressBookBusinessException extends BaseException {//这句话的意思是AddressBookBusinessException继承BaseException，作用是：AddressBookBusinessException可以使用BaseException的方法，BaseException用于处理异常
 
-    public AddressBookBusinessException(String msg) {
-        super(msg);
+    public AddressBookBusinessException(String msg) {//这句话的意思是AddressBookBusinessException的构造方法，作用是：当AddressBookBusinessException被实例化时，可以传入一个字符串，这个字符串是异常信息
+        super(msg);//这句话的意思是调用父类的构造方法，作用是：将传入的字符串传给父类的构造方法
     }
 
 }
---
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -0,0 +1,35 @@
+package com.sky.controller.user;
+
+
+import com.sky.dto.OrdersDTO;
+import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.Result;
+import com.sky.service.OrderService;
+import com.sky.vo.OrderSubmitVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("userOrderController")//由于管理端也会定义同样的类，这里起了一个别名
+@RequestMapping("/user/order")
+@Api(tags = "用户端订单相关接口")
+@Slf4j
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    /*用户下单*/
+    @PostMapping("/submit")
+    @ApiOperation("用户下单接口")
+    public Result<OrderSubmitVO> submit(@RequestBody OrdersSubmitDTO ordersSubmitDTO){
+        log.info("用户下单:{}",ordersSubmitDTO);
+        OrderSubmitVO orderSubmitVO= orderService.submitOrder(ordersSubmitDTO);
+        return Result.success(orderSubmitVO);
+    }
+}
